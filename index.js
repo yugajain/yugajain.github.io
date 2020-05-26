1d76ef099a96c5509342cd877da95fb29ab025c1
yugajain <allrounder0091@gmail.com>
2020-05-26 20:58:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-
-
-let subStatus = prompt('are you subscribed to All Rounder?');
-document.getElementById('someText').innerHTML = 'subscription status: ' + subStatus;
-
-let name = prompt('name: ')
-document.getElementById('name').innerHTML = 'name: ' + name
-
-let age = prompt('age')
-document.getElementById('KuchBhiText').innerHTML = 'Your age:' + age
-
-
-
 alert('There Is a news section after the videos section. It is updated every 2 days ')
